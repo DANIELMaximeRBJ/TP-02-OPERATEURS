2a1ab17dd09162cb1057afb6f2d62db7c18cda84
Maxime.DANIEL <m.daniel2009@hotmail.fr>
2018-03-19 12:46:14 +0100

    tp2 debut

--- src/main/java/fr/operateurs/console/OperateursApp.java
+++ src/main/java/fr/operateurs/console/OperateursApp.java
@@ -1,9 +1,23 @@
 package fr.operateurs.console;
 
+import java.util.Scanner;
 
 public class OperateursApp {
 	 public static void main( String[] args )
 	    {
-
+		 	System.out.println("***** Application Opérateurs *****");
+		 	
+		 	System.out.println("Veuillez saisir le premier nombre :");
+		 	Scanner questionUser = new Scanner(System.in) ;
+		 	int nombre_1 = questionUser.nextInt() ; 
+		 	
+		 	System.out.println("Veuillez saisir le second nombre :");
+		 	int nombre_2 = questionUser.nextInt() ; 
+		 	
+		 	System.out.println(nombre_1 + "+" + nombre_2 + "=" + (nombre_1 + nombre_2));
+		 	System.out.println(nombre_1 + "-" + nombre_2 + "=" + (nombre_1 - nombre_2));
+		 	System.out.println(nombre_1 + "*" + nombre_2 + "=" + (nombre_1 * nombre_2));
+		 	System.out.println(nombre_1 + "/" + nombre_2 + "=" + (nombre_1 / nombre_2));
+		 	System.out.println(nombre_1 + "%" + nombre_2 + "=" + (nombre_1 % nombre_2));
 	    }
 }
